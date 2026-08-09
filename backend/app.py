afada226bb980fc5f127420beb154e000351ded1
Nicholas-Smith463 <Nicholas.Smith463@gmail.com>
2026-08-08 23:59:48 -0400

    Refactor waiver form and validation logic; add styles and remove old index.html

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://baselinesports.ca/waiver"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -14,6 +15,13 @@ app.add_middleware(
 @app.post("/baseline-waiver-test/submit")
 async def submit(data: dict):
     print(data)
+
+    # adult check
+    if (datetime.now() - datetime.fromisoformat(data["dob"])).days/365 <= 18 and not "guard-ack" in data:
+        return {"success": False, "error": "Participants under 18 must have a parent or guardian fill out the minor waiver"}
+
+    # emergency contact check
+
 
     # save to database
     # send email confirmation
